158278526376baee08ebc86111dd3818f5701a99
seung-ddak <seunghoon1113@naver.com>
2024-12-07 15:46:47 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv(override=True) 
+# from dotenv import load_dotenv
+# load_dotenv(override=True) 
 
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
